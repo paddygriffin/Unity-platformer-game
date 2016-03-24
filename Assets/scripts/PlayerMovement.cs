74dd54f32cbcafa1b6c51f6d87c3dec3a8f5627e
unknown <G00314635@CR308.gmadtree.gmit.ie>
2016-03-24 19:10:13 +0000

    basic movement

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//variables
+	public float speed;
+	public float jump;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown (KeyCode.Space))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0 , jump);
+		}
+
+		if(Input.GetKey (KeyCode.D))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(speed , 0);
+		}
+
+		if(Input.GetKey (KeyCode.A))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-speed , 0);
+		}
+	
+	}
+}
