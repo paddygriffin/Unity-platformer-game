99992950b48e84b6d446626e78c141b7b02d8662
paddygriffin <G00314635@gmit.ie>
2016-03-29 20:20:24 +0100

    updating checkpoints

--- Assets/scripts/checkPoint.cs
+++ Assets/scripts/checkPoint.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class checkPoint : MonoBehaviour {
 
+	public levelManager levelManage;
+	
 	// Use this for initialization
 	void Start () {
-	
+		levelManage = FindObjectOfType<levelManager> (); // finds any object with levelmanager
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//detects when player enters a triger zone
+		if (other.name == "Player") {
+			levelManage.currentCheckpoint = gameObject;
+			
+		}
 	}
 }
