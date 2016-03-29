84b50d0c647b17c1aa77dee2635f555b0f4baed6
paddygriffin <G00314635@gmit.ie>
2016-03-29 17:04:12 +0100

    double jump
    movement, jumping, platform adjustment

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -7,27 +7,48 @@ public class PlayerMovement : MonoBehaviour {
 	public float speed;
 	public float jump;
 
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;//will show if player hits ground
+
+	private bool doubleJump;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	void FixedUpdate() {
+		
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (grounded) {
+			doubleJump = false;
+		}
 		
-		if(Input.GetKeyDown (KeyCode.Space))
+		if(Input.GetKeyDown (KeyCode.Space) && grounded) //player can now only jump when on the ground
 		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(0 , jump);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+		}
+
+		//double jump
+		if (Input.GetKeyDown (KeyCode.Space) && !doubleJump &&!grounded) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+			doubleJump = true;//able to jump twice 
 		}
 
 		if(Input.GetKey (KeyCode.D))
 		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(speed , 0);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 
 		if(Input.GetKey (KeyCode.A))
 		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-speed , 0);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 	
 	}
