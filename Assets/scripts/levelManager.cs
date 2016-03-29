f1fc7c08fd5e7f7f69378bc7895f25b98395bbed
paddygriffin <G00314635@gmit.ie>
2016-03-29 19:56:24 +0100

    respawn
    add checkpoints which respawn player after its killed

---
+++ Assets/scripts/levelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelManager : MonoBehaviour {
+
+	public GameObject currentCheckpoint;
+
+	private PlayerMovement player;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerMovement> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RespawnPlayer()
+	{
+		Debug.Log ("Player Respawn");
+		player.transform.position = currentCheckpoint.transform.position;
+	}
+}
---
+++ Assets/scripts/playerDestruction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerDestruction : MonoBehaviour {
+
+	public levelManager levelManage;
+
+	// Use this for initialization
+	void Start () {
+		levelManage = FindObjectOfType<levelManager> (); // finds any object with levelmanager
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		//detects when player enters a triger zone
+		if (other.name == "Player") {
+			levelManage.RespawnPlayer();//calling the respawn player
+
+		}
+	}
+}
