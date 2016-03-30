0480a9cb0c8a2dfddf22fc1d238e02a3564c529e
paddygriffin <G00314635@gmit.ie>
2016-03-30 20:39:11 +0100

    levels
    adding basic levels with different scenes and then adding each level to
    the build settings or otherwise an error will occur

---
+++ Assets/scripts/levelLoader.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelLoader : MonoBehaviour {
+
+	private bool zone; //to know if player is in the zone
+
+	public string levelLoad;
+
+	// Use this for initialization
+	void Start () {
+		zone = false; //as player is not in zone
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//entering new level 
+		if (Input.GetKeyDown (KeyCode.W) && zone) {
+			Application.LoadLevel(levelLoad); //immediate load level
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)//finds what has enter the level zone
+	{
+		if (other.name == "Player") {
+
+			zone = true; //the player is in the zone
+		}
+	}
+
+	//to avoid if a player wants to return back instead of entering the zone
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player") {
+			//player must press w to enter new level
+			zone = false; 
+		}
+	}
+}
