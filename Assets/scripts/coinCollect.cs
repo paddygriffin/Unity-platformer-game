c65c8afa95eb9751c6881db648e7c97874bc88c1
paddygriffin <G00314635@gmit.ie>
2016-03-30 18:09:35 +0100

    score
    adding a coin and adding onto the score

---
+++ Assets/scripts/coinCollect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinCollect : MonoBehaviour {
+
+	//how much coin is worth
+	public int points;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		//only player collects coins 
+		if (other.GetComponent<PlayerMovement> () == null)
+			return;
+
+		scoreManager.AddPoint (points); //calling scoreManager to add points
+
+		Destroy (gameObject); //remove coin 
+	}
+}
