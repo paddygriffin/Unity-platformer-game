14b08f3be48fd0e534c272111034685505526352
paddygriffin <G00314635@gmit.ie>
2016-03-31 15:42:17 +0100

    Lives
    working on the lives and the game over popup screen when player runs out
    of lives

---
+++ Assets/scripts/lifeManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class lifeManager : MonoBehaviour {
+
+	public int startingLives;
+	private int lifeCounter;
+
+	private Text theText;
+
+	public GameObject gameOver;
+
+	public PlayerMovement player;
+
+
+	// Use this for initialization
+	void Start () {
+		theText = GetComponent<Text>();//find text object and assaign it
+
+		lifeCounter = startingLives; 
+
+		player = FindObjectOfType<PlayerMovement> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lifeCounter <= 0) {
+
+			gameOver.SetActive(true);
+			player.gameObject.SetActive(false);
+		}
+
+
+
+		theText.text = " " + lifeCounter;
+	
+	}
+
+	public void GiveLife()
+	{
+		lifeCounter++;
+	}
+
+	public void TakeLife()
+	{
+		lifeCounter--;
+	}
+}
--- Assets/scripts/playerDestruction.cs
+++ Assets/scripts/playerDestruction.cs
@@ -5,15 +5,18 @@ public class playerDestruction : MonoBehaviour {
 
 	public levelManager levelManage;
 
+	private lifeManager lifeSystem;
+
 	// Use this for initialization
 	void Start () {
 		levelManage = FindObjectOfType<levelManager> (); // finds any object with levelmanager
-	
+		lifeSystem = FindObjectOfType<lifeManager> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
 	}
 
 
@@ -21,6 +24,7 @@ public class playerDestruction : MonoBehaviour {
 		//detects when player enters a triger zone
 		if (other.name == "Player") {
 			levelManage.RespawnPlayer();//calling the respawn player
+			lifeSystem.TakeLife();
 
 		}
 	}
