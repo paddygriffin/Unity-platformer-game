525a549390b87634f9928252709d3ed216fdea21
paddygriffin <G00314635@gmit.ie>
2016-03-31 18:50:12 +0100

    adding a menu
    adding a menu. However, need to fix the levels and the lives on each
    level..also adding a build to play game

---
+++ Assets/scripts/menuSelect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuSelect : MonoBehaviour {
+
+	public string startlevel;
+
+
+	//functions for each button
+	public void NewGame()
+	{
+		Application.LoadLevel (startlevel);
+	}
+	
+	public void QuitGame()
+	{
+		Debug.Log ("Game exiteded");
+		Application.Quit ();
+	}
+}
